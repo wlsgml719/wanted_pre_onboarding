5d1d7ef608f51807ca04bd09ad03b9984443d2a3
강진희 <jinheekang@gangjinhuiui-MacBookAir.local>
2022-09-13 14:14:28 +0900

    FIX: .ignore files

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {};
 
-CONFIG.app = process.env.NODE_ENV
+CONFIG.app = process.env.NODE_ENV || 'development';
+CONFIG.app_name = 'wanted_onboarding';
 CONFIG.port = process.env.PORT || '3900';
 
 CONFIG.scheme = process.env.SCHEME || 'http';
